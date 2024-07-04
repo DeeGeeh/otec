5f009dedb8e5be8ecd7864c8c4c3067ebf8a23d4
Diar <diar.ghaderi@tuni.fi>
2024-07-04 17:40:59 +0300

    Did some stuff

--- caesar/main.c
+++ caesar/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include "caesar.h"
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    
+    char *mj = argv[1];
+    char *aakkosto = argv[2];
+    char *korvaavat = argv[3];
+
+    caesarKoodaa(mj, aakkosto, korvaavat);
+    printf("%s\n", mj);
+
     return 0;
 }
