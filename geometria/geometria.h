3c546ad66ea1168307d9ee42d9bdbf81cf236a43
Diar <diar.ghaderi@tuni.fi>
2024-07-10 01:45:21 +0300

    clean up

--- geometria/geometria.h
+++
@@ -1,9 +0,0 @@
-#ifndef GEOMETRIA_H
-#define GEOMETRIA_H
-
-extern const long double PII;
-
-double ympyranAla(double sade);
-double pallonTilavuus(double sade);
-
-#endif /* GEOMETRIA_H */
--- muunto/muunto.h
+++
@@ -1,6 +0,0 @@
-#ifndef MUUNTO_H
-#define MUUNTO_H
-
-void muunna(char mj[]);
-
-#endif /* MUUNTO_H */
