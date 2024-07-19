677b997ba7089d7af4e0284ebd7a3770a149b126
Diar <diar.ghaderi@tuni.fi>
2024-07-20 01:08:36 +0300

    init file

---
+++ h2/kuukaudet/kuukaudet.c
@@ -0,0 +1,8 @@
+/*
+ * COMP.CS.120 Ohjelmoinnin Tekniikka C
+ *
+ * Diar Ghaderi (152098520)
+ * diar.ghaderi@tuni.fi
+ */
+
+#include "kuukaudet.h"
---
+++ h2/kuukaudet/kuukaudet.h
@@ -0,0 +1,15 @@
+/*
+ * COMP.CS.120 Ohjelmoinnin Tekniikka C
+ *
+ * Diar Ghaderi (152098520)
+ * diar.ghaderi@tuni.fi
+ */
+
+#include <stdio.h>
+
+#ifndef kuukaudet.h
+#define kuukaudet.h
+
+
+
+#endif
